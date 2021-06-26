05caaf460ce8558c531b0bfda99564c447ccc50c
0-jagadeesh-0 <jagadeeshkondra1@gmail.com>
2021-06-26 10:56:27 +0530

    adding variables and datatypes

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+# Data Types
+
+
+x = 2 # int
+y = 2.5 # float
+z = 3+1j # complex
+name = "Jagadeesh" # str
+
---
+++ variables.py
@@ -0,0 +1,24 @@
+# A variable is created the moment you first assign a value to it.
+
+num = 3
+decimal = 2.3
+name = "Jagadeesh"
+y = True
+
+print(type(num)) # num is of int type.
+print(type(decimal)) # decimal is of float type. 
+print(type(name)) # name is of string type. 
+print(type(y)) # y is of bool type. 
+
+# Type Casting
+
+a = int(3) # a is int type. 
+b = float(5) # b is float type. 
+c = str(6) # c is str type.
+d = bool("a") # d is bool type. 
+
+# Multiple Variables
+
+x,z="apple","banana"
+print(x) # apple
+print(z) # banana
