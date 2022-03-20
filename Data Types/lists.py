e1b7bcf0accd01e8f2a581257a34a231eea75437
Jagadeesh <74336885+0-jagadeesh-0@users.noreply.github.com>
2022-03-20 10:09:01 +0530

    adding data types

---
+++ Data Types/lists.py
@@ -0,0 +1,18 @@
+# List
+# Lists are used to store multiple items in a single variable.
+
+
+# Creating Lists
+
+fruits = ['apple', 'banana', 'guava']
+print(fruits)
+
+# Lists are mutable
+
+fruits[0] = 'grapes'
+
+print(fruits)
+
+# Length of list
+
+print(len(fruits))
